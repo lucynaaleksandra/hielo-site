e9e9eb6ce0ca333a743cc6cfaad8055539926b62
Lucyna Aleksandra <lucy@lucynaaleksandra.com>
2018-12-21 20:12:56 -0500

    finished slider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,62 @@
 import "./index.scss";
 
 // SIDEBAR
+window.openNav = () => {
+  document
+    .querySelector(".la-nav__sidebar")
+    .classList.add("la-nav__sidebar--visible");
+};
 
-export function openNav() {
-  document.getElementById("mySidenav").style.width = "200px";
+window.closeNav = () => {
+  document
+    .querySelector(".la-nav__sidebar")
+    .classList.remove("la-nav__sidebar--visible");
+};
+
+// SLIDESHOW
+let slideIndex = 1;
+showSlides(slideIndex);
+
+window.currentSlide = n => {
+  showSlides((slideIndex = n));
+};
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("la-nav__slide");
+  let dots = document.getElementsByClassName("la-nav__slider");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
-export function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+// STICKY NAV
+// window.onscroll = function() {
+//   myFunction();
+// };
+
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
